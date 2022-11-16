b189bbc198441610bcc9fda35175fe16545c7309
Mari <mrs.tereshchenko@gmail.com>
2022-11-16 16:15:26 +0100

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const setWatchingTime = ({ seconds }) => {
+   localStorage.setItem('videoplayer-current-time', seconds);
+};
+player.on('timeupdate', throttle(setWatchingTime, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+console.log(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const formData = {};
+const STORAGE_KYE = 'feedback-form-state';
+
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+fillInFormInput();
+
+function onInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KYE, JSON.stringify(formData));
+    
+}
+function fillInFormInput(el) {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KYE));
+
+    if (savedData) {
+    
+    }
+    console.log(Object.values(savedData));
+    
+}
+
+
+function onFormSubmit(event) {
+    event.preventDefault(); 
+    if (email.value === "" || message.value === "") {
+        alert('Всі поля мають бути заповнені');  
+    }
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KYE)));
+    event.target.reset();
+    localStorage.removeItem(STORAGE_KYE);
+}
+
+
+
+
 
